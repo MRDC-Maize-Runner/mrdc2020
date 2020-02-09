bb1117eff7c7ae785545f09ef4f338ec8e6051b9
Abraham Zukor <abezukor@gmail.com>
2020-02-09 12:43:13 -0600

    working on serial

--- controllerPC/src/main.rs
+++ controllerPC/src/main.rs
@@ -1,62 +1,51 @@
-use gilrs::{Axis, Event, Gilrs};
-use std::sync::mpsc::sync_channel;
-use std::sync::mpsc::{Receiver, SyncSender};
+use gilrs::Axis;
+use std::sync::mpsc::{channel, sync_channel};
+use std::sync::mpsc::{Receiver, Sender, SyncSender};
 use std::thread;
 
+use serialport::SerialPort;
+
+mod controller;
+mod serial;
+
 fn main() {
+    /*
     //make a channel
-    let (tx, rx): (SyncSender<(f32, f32, u64)>, Receiver<(f32, f32, u64)>) = sync_channel(0);
+    let (controller_tx, controller_rx): (SyncSender<(f32, f32, u64)>, Receiver<(f32, f32, u64)>) =
+        sync_channel(0);
 
     //spawn a controller thread
-    let controller_thread = thread::spawn(move || {
-        controller(Axis::LeftStickX, Axis::LeftStickY, tx);
+    let _controller_thread = thread::spawn(move || {
+        controller::controller_loop(Axis::LeftStickX, Axis::LeftStickY, controller_tx);
     });
 
+    let (serial_transmit_port, serial_receive_port) =
+        serial::port_setup("/dev/ttyUSB0", 115200, 10);
+
+    let (serial_transmit_tx, serial_transmit_rx): (
+        Sender<(f32, f32, Vec<bool>)>,
+        Receiver<(f32, f32, Vec<bool>)>,
+    ) = channel();
+    let (serial_receive_tx, serial_receive_rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+
+    let _serial_transmit_thread =
+        thread::spawn(move || serial::serial_send_data(serial_transmit_port, serial_transmit_rx));
+    let _serial_receive_thread =
+        thread::spawn(move || serial::serial_get_data(serial_receive_port, serial_receive_tx));
+    */
+    println!(
+        "{:?}",
+        serial::construct_message((1.1, -1.1, vec![true, false, false, false, false, true, true, true, true]))
+    );
     //main loop
     loop {
         //get data from the challen
-        let received = rx.recv().unwrap();
+        //let received = controller_rx.recv().unwrap();
         //print it
-        println!("Got: {:?}", received);
+        //println!("Got: {:?}", received);
 
         thread::sleep_ms(1); // sleep for 100ms
     }
 
     //controller_thread.join().unwrap(); //this wont run
-}
-
-//Function to run a game controller
-fn controller(foward: Axis, turn: Axis, tx: SyncSender<(f32, f32, u64)>) {
-    //initialize the library
-    let mut gilrs = Gilrs::new().unwrap();
-    // Iterate over all connected gamepads and print out stuff about them
-    for (_id, gamepad) in gilrs.gamepads() {
-        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
-    }
-
-    //make a place to store the active gamepad
-    let mut active_gamepad = None;
-
-    //find the active gamepad by looking for an event
-    while active_gamepad.is_none() {
-        while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
-        }
-    }
-
-    loop {
-        /* Get the latest data from the gamepad. This is the only way i could figure out how to do this.
-        It basically just goes through all the controller events until nothing more in the buffer.Receiver
-        This is what we want because we want the current state of the joystick not a past state
-        I though about making this event based but then i would have to store the last state of everything. */
-        while let Some(_ev) = gilrs.next_event() {}
-
-        //Send the current state of the gamepad over the channel.
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            let analog = (gamepad.value(foward), gamepad.value(turn), gilrs.counter());
-            tx.send(analog).unwrap();
-        }
-        gilrs.inc();
-    }
 }
---
+++ controllerPC/src/serial.rs
@@ -0,0 +1,78 @@
+use std::io::{self, Write};
+use std::time::Duration;
+
+use std::sync::mpsc::{Receiver, Sender};
+
+use serialport::prelude::*;
+
+use std::mem::transmute;
+
+extern crate serialport;
+
+pub fn port_setup(
+    port_name: &str,
+    baud_rate: u32,
+    timeout: u32,
+) -> (
+    Box<dyn serialport::SerialPort>,
+    Box<dyn serialport::SerialPort>,
+) {
+    let mut settings: SerialPortSettings = Default::default();
+    settings.timeout = Duration::from_millis(10);
+    settings.baud_rate = baud_rate;
+
+    let mut transmit_port =
+        serialport::open_with_settings(&port_name, &settings).expect("Failed to serial open port");
+    let mut receive_port = transmit_port.try_clone().expect("Failed to clone");
+
+    return (transmit_port, receive_port);
+}
+
+pub fn serial_send_data(
+    mut port: Box<dyn serialport::SerialPort>,
+    rx: Receiver<(f32, f32, Vec<bool>)>,
+) {
+    loop {
+        let message: (f32, f32, Vec<bool>) = rx.recv().unwrap();
+        let bytes: Vec<u8> = construct_message(message);
+        port.write(&bytes).expect("Cant write");
+    }
+}
+fn bool_to_u8(b: bool) -> u8 {
+    match b {
+        true => 1,
+        false => 0,
+    }
+}
+
+pub fn construct_message(message: (f32, f32, Vec<bool>)) -> Vec<u8> {
+    let mut bytes: Vec<u8> = Vec::new();
+    let mut bytes_from_float: [u8; 4] = unsafe { transmute(message.0.to_bits().to_be()) };
+    bytes.extend(bytes_from_float.iter().cloned());
+    bytes_from_float = unsafe { transmute(message.1.to_bits().to_be()) };
+    bytes.extend(bytes_from_float.iter().cloned());
+
+    let mut bit_num = 8;
+    let mut byte: u8 = 0;
+    for x in message.2 {
+        println!("{}, {:b}", x, byte);
+        byte += bool_to_u8(x);
+        bit_num += -1;
+        if bit_num == 0 {
+            bytes.push(byte);
+            byte = 0;
+            bit_num = 7;
+        } else {
+            byte = byte << 1;
+        }
+	}
+	bytes.push(byte);
+    return bytes;
+}
+
+pub fn serial_get_data(mut port: Box<dyn serialport::SerialPort>, tx: Sender<Vec<u8>>) {
+    let mut serial_buf = Vec::new();
+    loop {
+        port.read(serial_buf.as_mut_slice()).expect("boom");
+    }
+}
